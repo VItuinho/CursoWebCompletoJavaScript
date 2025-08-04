acc6ac430b564b8dcd807df3a46b4e839b1629aa
Vituinho <trabalhovictor531@gmail.com>
2025-08-04 11:03:39 -0300

    Game Mata Mosquito - Criando Posições Randomicas

--- app2/jogo.js
+++ app2/jogo.js
@@ -13,15 +13,21 @@ ajustaTamanhoPalcoJogo()
 
 function posicaoRandomica() {
 
-    var posicaoX = Math.floor(Math.random() * largura)
-    var posicaoY = Math.floor(Math.random() * altura)
+    var posicaoX = Math.floor(Math.random() * largura) - 90
+    var posicaoY = Math.floor(Math.random() * altura) - 90
     console.log(posicaoX, posicaoY)
+
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
 
     //criar o elemento html
 
     var mosquito = document.createElement('img')
     mosquito.src = 'imagens/mosca.png'
     mosquito.className = 'mosquito1'
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
 
     document.body.appendChild(mosquito)
 }

133861855f866f702707f0299f0f31c15b24a7cc
Vituinho <trabalhovictor531@gmail.com>
2025-08-04 11:13:15 -0300

    Game Mata Mosquito - Lado A e Lado B

--- app2/jogo.js
+++ app2/jogo.js
@@ -24,7 +24,7 @@ function posicaoRandomica() {
 
     var mosquito = document.createElement('img')
     mosquito.src = 'imagens/mosca.png'
-    mosquito.className = tamanhoAleatorio()
+    mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
     mosquito.style.left = posicaoX + 'px'
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
@@ -47,4 +47,15 @@ function tamanhoAleatorio() {
             return 'mosquito3'
     }
 
+}
+
+function ladoAleatorio() {
+    var classe = Math.floor(Math.random() * 2)
+
+    switch (classe) {
+        case 0:
+            return 'ladoA'
+        case 1:
+            return 'ladoB'
+    }
 }
